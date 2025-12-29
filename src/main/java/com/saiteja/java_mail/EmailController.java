99cfc0b9e25c9540210956d8dfb56d10d445f1c0
daggusaiteja961911 <daggusaiteja123@gmail.com>
2025-12-29 12:34:16 +0530

    added html side

--- src/main/java/com/saiteja/java_mail/EmailController.java
+++ src/main/java/com/saiteja/java_mail/EmailController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.mail.internet.MimeMessage;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 
 @RestController
 public class EmailController {
@@ -54,4 +58,46 @@ public class EmailController {
 			return e.getMessage();
 		}
 	}
+	
+	@RequestMapping("/send-html-email")
+	public String sendHtmlEmailApi() {
+	    try {
+	        sendHtmlEmail(
+	                "nitwarangal219@gmail.com",
+	                "Sai Teja",
+	                "456789"
+	        );
+	        return "HTML Email sent successfully!";
+	    } catch (Exception e) {
+	        return e.getMessage();
+	    }
+	}
+
+	
+	
+	public String loadEmailTemplate(String name, String otp) throws IOException {
+	    String html = Files.readString(
+	        Paths.get("src/main/resources/templates/email-template.html")
+	    );
+	    html = html.replace("${name}", name);
+	    html = html.replace("${otp}", otp);
+
+	    return html;
+	}
+
+	public void sendHtmlEmail(String to, String name, String otp) throws Exception {
+	    MimeMessage message = mailSender.createMimeMessage();
+	    MimeMessageHelper helper =
+	            new MimeMessageHelper(message, true, "UTF-8");
+
+	    helper.setFrom("nitwarangal219@gmail.com");
+	    helper.setTo(to);
+	    helper.setSubject("Your OTP Verification");
+	    helper.setText(loadEmailTemplate(name, otp), true);
+
+	    mailSender.send(message);
+	}
+
+
+
 }
